4684100128ba2d13867029d915a84f55caccf164
Lars Urban <urbanswelt@gmail.com>
2012-12-26 19:13:43 +0100

    Final Test qemu

--- pi_server/cleanup.sh
+++ pi_server/cleanup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd
-rm pi_serv_clean.sh
-rm pkgs.txt
 echo Download Beginn
 wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pi_serv_clean.sh
 wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pkgs.txt
--- pi_server/pi_serv_clean.sh
+++ pi_server/pi_serv_clean.sh
@@ -4,4 +4,4 @@ while read p; do
 	temp=`echo $p | awk -F\  ' { print $2 } ' | awk -F: ' { print $1 } '`
 	data=`echo $data $temp`
 done < $1
-apt-get purge $data
+apt-get purge $data && apt-get autoremove && apt-get update && apt-get upgrade && rm pi_serv_clean.sh pkgs.txt cleanup.sh
