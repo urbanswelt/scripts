d60ac9a8458e92e9b9ae930f3972762d2f972b19
Lars Urban <urbanswelt@gmail.com>
2012-12-26 18:27:30 +0100

    failure in links for wget

--- pi_server/cleanup.sh
+++ pi_server/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd
-rm pi_serv_clean.sh pkgs.txt	\
-rm pkgs.txt	\
-wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pi_serv_clean.sh	\
-wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pkgs.txt	\
-chmod -R 770 pi_serv_clean.sh	\
+rm pi_serv_clean.sh pkgs.txt
+rm pkgs.txt
+wget -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pi_serv_clean.sh
+wget -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pkgs.txt
+chmod -R 770 pi_serv_clean.sh
 sudo ./pi_serv_clean.sh pkgs.txt
