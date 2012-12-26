d74e15729a009095ad278b977d71bbf894320ae9
Lars Urban <urbanswelt@gmail.com>
2012-12-26 20:05:46 +0100

    typo

--- pi_server/cleanup.sh
+++ pi_server/cleanup.sh
@@ -10,7 +10,7 @@ wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/mast
 wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pkgs.txt
 echo 'Download End'
 chmod -R 770 pi_serv_clean.sh
-echo 'Usage before cleaning'
+echo 'allocation before cleaning'
 df -lah | awk '$NF=="/"{print}'
 pause 'Press [Enter] key to continue...'
 sudo ./pi_serv_clean.sh pkgs.txt
--- pi_server/pi_serv_clean.sh
+++ pi_server/pi_serv_clean.sh
@@ -4,4 +4,4 @@ while read p; do
 	temp=`echo $p | awk -F\  ' { print $2 } ' | awk -F: ' { print $1 } '`
 	data=`echo $data $temp`
 done < $1
-apt-get purge $data && apt-get autoremove && apt-get update && apt-get upgrade && rm pkgs.txt cleanup.sh pi_serv_clean.sh && echo 'Usage after cleaning' && df -lah | awk '$NF=="/"{print}'
+apt-get purge $data && apt-get autoremove && apt-get update && apt-get upgrade && rm pkgs.txt cleanup.sh pi_serv_clean.sh && echo 'allocation after cleaning' && df -lah | awk '$NF=="/"{print}'
