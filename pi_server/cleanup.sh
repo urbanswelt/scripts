2fe683040a4498ac219a0743f422cbbb5e3ae1de
Lars Urban <urbanswelt@gmail.com>
2012-12-26 17:46:49 +0100

    one clean up script for better work

---
+++ pi_server/cleanup.sh
@@ -0,0 +1,7 @@
+cd
+rm pi_serv_clean.sh
+rm pkgs.txt
+wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pi_serv_clean.sh
+wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pkgs.txt
+chmod -R 770 pi_serv_clean.sh
+sudo ./pi_serv_clean.sh pkgs.txt
