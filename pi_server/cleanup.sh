83f0df47ee1f86638faa256da1e06aedd8745641
Lars Urban <urbanswelt@gmail.com>
2012-12-26 19:46:07 +0100

    formating

--- pi_server/cleanup.sh
+++ pi_server/cleanup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# init
+function pause(){
+   read -p "$*"
+}
+
 cd
-echo Download Beginn
+echo 'Download Beginn'
 wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pi_serv_clean.sh
 wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pkgs.txt
-echo Download End
+echo 'Download End'
 chmod -R 770 pi_serv_clean.sh
+echo 'Usage before cleaning'
+df -lah | awk '$NF=="/"{print}'
+pause 'Press [Enter] key to continue...'
 sudo ./pi_serv_clean.sh pkgs.txt
