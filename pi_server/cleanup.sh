2789265bb7f6e3de361f6e4b0e7d0886205309dc
Lars Urban <urbanswelt@gmail.com>
2012-12-26 19:02:27 +0100

    quiet run

--- pi_server/cleanup.sh
+++ pi_server/cleanup.sh
@@ -2,7 +2,9 @@
 cd
 rm pi_serv_clean.sh
 rm pkgs.txt
-wget -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pi_serv_clean.sh
-wget -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pkgs.txt
+echo Download Beginn
+wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pi_serv_clean.sh
+wget -q -N --no-check-certificate https://raw.github.com/urbanswelt/scripts/master/pi_server/pkgs.txt
+echo Download End
 chmod -R 770 pi_serv_clean.sh
 sudo ./pi_serv_clean.sh pkgs.txt
