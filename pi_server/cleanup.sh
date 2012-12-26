69fa537b190581f31149fc353b8c3d003a310078
Lars Urban <urbanswelt@gmail.com>
2012-12-26 18:09:04 +0100

    also one wget command

--- pi_server/cleanup.sh
+++ pi_server/cleanup.sh
@@ -1,7 +1,6 @@
 #Clean up Raspberry PI 
 cd
 rm pi_serv_clean.sh pkgs.txt
-wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pi_serv_clean.sh
-wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pkgs.txt
+wget -N --no-check-certificate https://github.com/urbanswelt/scripts/blob/master/pi_server/pi_serv_clean.sh https://github.com/urbanswelt/scripts/blob/master/pi_server/pkgs.txt
 chmod -R 770 pi_serv_clean.sh
 sudo ./pi_serv_clean.sh pkgs.txt
